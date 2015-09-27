f9c8baa6f9fe9bfffc1140f2cd9e8c13b175220a
wujiabin <wujiabin@wudeMacBook-Pro.local>
2015-09-28 00:51:16 +0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by wu jiabin on 15/9/28.
+//  Copyright (c) 2015年 wu jiabin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by wu jiabin on 15/9/28.
+//  Copyright (c) 2015年 wu jiabin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
