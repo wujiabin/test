a494ae45361eadae27059dd845aa2ff945d112dc
wujiabin <wujiabin@wudeMacBook-Pro.local>
2015-10-30 22:49:27 +0800

    test

--- test/ViewController.m
+++ test/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    [super view];
     [super viewDidLoad];  
     // Do any additional setup after loading the view, typically from a nib.
 }
